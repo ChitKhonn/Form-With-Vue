af5b1ccc9f8713e1dbd63afca5fa353e7d101b1b
ckc <chitkhonncho9@gmail.com>
2023-07-23 12:40:37 +0630

    making html form and css with vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
 
-  <h1>Form</h1>
-   
+  <div>
+    <h1>Form</h1>
+    <SignUpVue/>
+  </div>
   
 </template>
 
 <script>
-
+import SignUpVue from './components/SignUp.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    SignUpVue,
   }
 }
 </script>
@@ -24,5 +26,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background-color: #eee;
+  margin: 0;
 }
 </style>
